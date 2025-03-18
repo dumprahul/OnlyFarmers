f68e15eab87d73252e4bed2549b78e777a8dc2da
dumprahul <shanrahul04@gmail.com>
2025-03-19 02:30:37 +0530

    test3-sol

--- check3.sol
+++ check3.sol
@@ -19,29 +19,34 @@ contract FarmStaking {
     uint256 public constant EARLY_WITHDRAWAL_PENALTY = 10; // 10%
 
     address public owner;
+    address public constant FARMER_ADDRESS = 0x62752de9CA838C71084d2B841Be1Aa71e45B4B7e;
+    address public constant SUSTAINABLE_POOL_ADDRESS = 0x1B02525C23130f4f64D6d86a41e916b70e5E03aA;
+
     uint256 public totalStaked;
 
     event Staked(address indexed user, uint256 amount, uint256 yieldPercentage, uint256 time);
     event Withdrawn(address indexed user, uint256 amount, uint256 reward);
+    event PenaltyApplied(address indexed user, uint256 penaltyAmount);
 
     constructor() {
         owner = msg.sender;
     }
 
-    function stake(address user, uint256 amount, uint256 yieldPercentage, uint256 time) external {
+    function stake(uint256 amount, uint256 yieldPercentage) external {
         require(amount > 0, "Amount must be greater than zero");
         require(yieldPercentage >= 0 && yieldPercentage <= 100, "Invalid Yield Percentage");
+        require(!stakes[msg.sender].active, "User already has an active stake");
 
-        stakes[user] = StakeInfo({
-            user: user,
+        stakes[msg.sender] = StakeInfo({
+            user: msg.sender,
             amount: amount,
             yieldPercentage: yieldPercentage,
-            startTime: time,
+            startTime: block.timestamp,
             active: true
         });
 
         totalStaked += amount;
-        emit Staked(user, amount, yieldPercentage, time);
+        emit Staked(msg.sender, amount, yieldPercentage, block.timestamp);
     }
 
     function viewStake(address user) external view returns (StakeInfo memory) {
@@ -58,6 +63,7 @@ contract FarmStaking {
     function withdraw() external {
         StakeInfo storage stakeInfo = stakes[msg.sender];
         require(stakeInfo.active, "No active stake");
+
         uint256 stakedAmount = stakeInfo.amount;
         uint256 timeElapsed = block.timestamp - stakeInfo.startTime;
         uint256 apy = calculateAPY(stakeInfo.yieldPercentage);
@@ -68,20 +74,27 @@ contract FarmStaking {
         if (timeElapsed < EPOCH_DURATION) {
             penalty = (stakedAmount * EARLY_WITHDRAWAL_PENALTY) / 100;
             stakedAmount -= penalty;
-            farmerRewards[msg.sender] += penalty / 2; // 5% to farmer rewards
-            fineBalance[msg.sender] += penalty / 2; // 5% to fine balance
+
+            uint256 farmerCut = penalty / 2;
+            uint256 sustainableCut = penalty / 2;
+
+            farmerRewards[FARMER_ADDRESS] += farmerCut;
+            sustainablePool[SUSTAINABLE_POOL_ADDRESS] += sustainableCut;
+            fineBalance[msg.sender] += penalty;
+
+            emit PenaltyApplied(msg.sender, penalty);
         }
-        
+
         uint256 userReward = (rewardAmount * 80) / 100;
         uint256 farmerReward = (rewardAmount * 17) / 100;
         uint256 sustainabilityReward = (rewardAmount * 3) / 100;
 
-        farmerRewards[msg.sender] += farmerReward;
-        sustainablePool[msg.sender] += sustainabilityReward;
-        
+        farmerRewards[FARMER_ADDRESS] += farmerReward;
+        sustainablePool[SUSTAINABLE_POOL_ADDRESS] += sustainabilityReward;
+
         delete stakes[msg.sender];
         totalStaked -= stakeInfo.amount;
-        
+
         emit Withdrawn(msg.sender, stakedAmount, userReward);
     }
 }
