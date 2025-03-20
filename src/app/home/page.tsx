546ff04b3b71bb17a42040706b26911ef5e89c1d
dumprahul <shanrahul04@gmail.com>
2025-03-21 01:31:23 +0530

    get-protocol-metrics

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
           <hr className="border-gray-300 dark:border-neutral-700 mb-4" />
     <footer className="w-full bg-black text-white py-10">
   <div className="max-w-7xl mx-auto px-6 text-center">
-    <h3 className="text-xl md:text-2xl font-semibold">PiCore</h3>
+    <h3 className="text-xl md:text-2xl font-semibold">OnlyFarmers</h3>
     <p className="text-sm md:text-base text-neutral-400 mt-2">
     BTCFi - AI - ZKP - Compute
     </p>
